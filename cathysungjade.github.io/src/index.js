b5b149de67651cfbbafe141fd917c40a296af3bc
cathysungjade <51140762+cathysungjade@users.noreply.github.com>
2020-09-01 14:42:29 -0400

    update

--- cathysungjade.github.io/src/index.js
+++ cathysungjade.github.io/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, Route } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>,
+   document.getElementById('root'))
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker()
